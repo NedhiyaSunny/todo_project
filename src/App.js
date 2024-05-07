45cce860759563ea0d8f6874ab009322d9ceb53b
nedhiyasunny <nedhiyasunny2018@gmail.com>
2024-05-07 12:58:28 +0530

    Create a React app and publish it to Github Pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import HelloWorld from "./components/screens/HelloWorld";
+// import Basic from "./components/screens/Basic";
+
+class App extends Component {
+    render(){
+        return <HelloWorld />
+    }
+}
+
+export default App;
+
